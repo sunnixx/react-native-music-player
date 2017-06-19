2c310b6e1ed8be616eb8a3c52ff9a0e9a4a470b6
Sumair Baloch <Sumair Baloch>
2017-06-20 00:35:13 +0500

    fetch data from server and added card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albums: []
+    };
+  }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => {
+      this.setState({ albums: response.data });
+    });
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album) => {
+      return <AlbumDetail key={album.title} album={album} />;
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
