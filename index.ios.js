e7fdd0c9c4aa420bfaa5f6b25508a62c4d680adc
Sumair Baloch <Sumair Baloch>
2017-06-22 11:49:18 +0500

    final styling complete with cards and buttons

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+//Import libs to help create components
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+const App = () => (
+  <View>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
+);
+
+//Render it to the device
+AppRegistry.registerComponent('album', () => App);
